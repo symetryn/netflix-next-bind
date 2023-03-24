6b1b0f802f1853088364d1b19ef129245ba96235
Rojan Maharjan <roseonmhr@gmail.com>
2023-03-24 08:07:53 +0545

    feat: add seamless click

--- shortcut.js
+++ shortcut.js
@@ -5,6 +5,9 @@ document.addEventListener('keypress', (event) => {
     var name = event.key;
     if (name === "n") {
         const nextButton = document.querySelector('[data-uia="control-next"]');
-        nextButton.click()
+        nextButton?.click()
+
+        const nextSeamlessButton = document.querySelector('[data-uia="next-episode-seamless-button"]');
+        nextSeamlessButton?.click()
     }
 }, false);
